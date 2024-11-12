005db4a323999266b05d5b81c8484ab61fbbb9bc
sunzhiqiang <80050573@xfmgroup.com>
2024-11-12 17:34:34 +0800

    初始化

---
+++ packages/examples/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import locale from 'element-plus/es/locale/lang/zh-cn'
+import vue3CronPlus from '../vue3-cron-plus/index'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+app.component(key, component)
+}
+app.use(ElementPlus, { locale })
+
+app.use(vue3CronPlus)
+
+app.mount('#app')

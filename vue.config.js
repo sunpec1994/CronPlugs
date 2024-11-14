7e3999b38112c6f50a62970e436b9ef502a5a4e6
sunzhiqiang <80050573@xfmgroup.com>
2024-11-14 10:14:23 +0800

    youhua

--- vue.config.js
+++ vue.config.js
@@ -24,27 +24,12 @@ module.exports = defineConfig({
       filename: "index.html"
     }
   },
-  transpileDependencies: false,
-  publicPath: process.env.NODE_ENV === 'production' ? './' : './', // 开发环境与生产环境的区分
-  // 扩展 webpack 配置，使 packages 加入编译
   chainWebpack: config => {
     config.module
-      .rule('eslint')
-      .exclude.add(path.resolve('lib'))
-      .end()
-
-    config.module
       .rule('js')
-      .include
-      .add('/lib/')
-      .end()
       .include.add(/examples/)
       .end()
       .use('babel')
       .loader('babel-loader')
-      .tap(options => {
-        // 修改它的选项...
-        return options
-      })
   },
 })
